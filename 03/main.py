5c20fb4005af924eb0c880c3e0be81578f30b4cf
rune <jamesbedford@prontonmail.com>
2024-12-17 12:06:51 +1100

    Solved day 3 part 2.

--- 03/main.py
+++ 03/main.py
@@ -2,11 +2,9 @@ import re
 import math
 
 
-def part1(corrupted_code: str):
+def sum_valid_multiples(corrupted_code: str) -> int:
     regex = r"mul\(([0-9]|[a1-9][0-9]|[a1-9][0-9][0-9]),([0-9]|[a1-9][0-9]|[a1-9][0-9][0-9])\)"
     valid_multiples = re.findall(regex, corrupted_code)
-
-    number_regex = r"\d{1,3}"
     total = 0
 
     for multiple in valid_multiples:      
@@ -16,9 +14,36 @@ def part1(corrupted_code: str):
     return total
 
 
+def find_enabled_code(corrupted_code: str) -> str:
+    enabled = True
+    enabled_code = ""
+
+    for i, c in enumerate(corrupted_code):
+        if corrupted_code[i:i + 4] == "do()":
+            enabled = True
+        elif corrupted_code[i:i + 7] == "don't()":
+            enabled = False
+
+        if enabled:
+            enabled_code += c
+
+    return enabled_code
+
+
+def part2(corrupted_code: str) -> int:
+    enabled_code = find_enabled_code(corrupted_code)
+
+    return sum_valid_multiples(enabled_code)
+
+
+def part1(corrupted_code: str) -> int:
+    return sum_valid_multiples(corrupted_code)
+
+
 def main():
     with open("03/input_03.txt") as f: memory_text = f.read()
-    print(part1(memory_text))    
+    print(part1(memory_text))
+    print(part2(memory_text))  
 
 
 if __name__ == "__main__":

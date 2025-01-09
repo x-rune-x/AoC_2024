06527253de100e1f9006f5331424778fdacf146e
rune[safe] <jamesbedford@protonmail.com>
2025-01-09 21:31:46 +1100

    New approach for Day 5 part 2. Still working for test data but not for real input

--- 05/main.py
+++ 05/main.py
@@ -1,29 +1,54 @@
-def check_pages_are_in_order(update: list[int], order_rules: list[tuple[int]]) -> bool:    
+def check_pages_are_in_order(update: list[int], order_rules: list[tuple[int]], verbose: bool = False) -> bool:    
     for rule in order_rules:
         first_num = rule[0]
         second_num = rule[1]
         if first_num in update and second_num in update and update.index(first_num) > update.index(second_num):
+            if verbose:
+                print(rule)
             return False
 
     return True
 
 
-def part1(order_rules: list[tuple[int]], updates: list[int]) -> int:
+def get_order(order_rules: list[tuple[int]]):
+    order = []
+    for rule in order_rules:
+        if rule[0] not in order:
+            order.append(rule[0])
+        if rule[1] not in order:
+            order.append(rule[1])
+        
+        pos_num1 = order.index(rule[0])
+        pos_num2 = order.index(rule[1])
+
+        if pos_num1 > pos_num2:
+            order.pop(pos_num2)
+            order.insert(pos_num1, rule[1])
+
+    return order
+
+
+def order_update(update: list[int], order: list[int]):
+    return [page for page in order if page in update]
+
+
+def part1(order_rules: list[tuple[int]], updates: list[list[int]]) -> int:
     correctly_ordered_updates = [update for update in updates if check_pages_are_in_order(update, order_rules)]
 
     return sum([update[len(update) // 2] for update in correctly_ordered_updates])
 
 
-def part2(order_rules: list[tuple[int]], updates: list[int]):
+def part2(order_rules: list[tuple[int]], updates: list[list[int]]):
+    order = get_order(order_rules)
     incorrectly_ordered_updates = [update for update in updates if check_pages_are_in_order(update, order_rules) == False]
 
+    total = 0
+
     for update in incorrectly_ordered_updates:
-        for rule in order_rules:          
-            if rule[0] in update and rule[1] in update and update.index(rule[0]) > update.index(rule[1]):
-                num = update.pop(update.index(rule[1]))
-                update.insert(update.index(rule[0]) + 1, num)
-    
-    return sum([update[len(update) // 2] for update in incorrectly_ordered_updates])
+        middle_page = [page for page in order if page in update][len(update) // 2]
+        total += middle_page
+
+    return total
 
 
 def process_input(input_path: str):
@@ -42,7 +67,7 @@ def process_input(input_path: str):
 
 
 def main():
-    input = process_input("05/test_05.txt")
+    input = process_input("05/input_05.txt")
     #print(part1(input[0], input[1]))
     print(part2(input[0], input[1]))
 

3606ce9a6868bbf69e0e9281161bb8eecbfe14db
rune[safe] <jamesbedford@protonmail.com>
2025-04-04 23:53:21 +1100

    Slightly wrong for day 6 part 1.

--- 06/main.py
+++ 06/main.py
@@ -1,3 +1,5 @@
+from sympy import sin, cos, pi
+
 def find_initial_position_of_guard(area_map: list[list[str]]) -> tuple[int, int]:
     # Assuming guard always starts facing up.
 
@@ -8,24 +10,29 @@ def find_initial_position_of_guard(area_map: list[list[str]]) -> tuple[int, int]
     
     raise Exception("A guard was not present")
 
+
 def part1(area_map):
     current_pos = find_initial_position_of_guard(area_map)
+    current_direction = pi / 2
     count = 1
 
-    while (current_pos[0] > -1 and current_pos[0] < len(area_map)) and (current_pos[1] > -1 and current_pos[1] < len(area_map[0])):
-        x = 1
+    while True:
+        current_pos = (current_pos[0] - sin(current_direction), current_pos[1] + cos(current_direction))
+        if current_pos[0] < 0 or current_pos[0] >= len(area_map) or current_pos[1] < 0 or current_pos[1] >= len(area_map[0]):
+            break # The guard has exited the boundary of the mapped area.
+
+        if area_map[current_pos[0]][current_pos[1]] == "#":
+            current_pos = (current_pos[0] + sin(current_direction), current_pos[1] - cos(current_direction)) # Hit obstacle so have to go back
+            current_direction -= (pi / 2) # Direction of guard rotates 90 degrees clockwise when they hit an obstacle.
+        else:
+            count += 1
     
     return count
 
 
-def load_input(input_path):
-    with open(input_path) as f: area_map = [line for line in f]
-
-    return area_map
-
-
 def main():
-    area_map = load_input("input.txt")
+    with open("06/test_06.txt") as f: area_map = [line.strip() for line in f]
+    print(part1(area_map))
 
 
 

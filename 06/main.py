6ade3c9525473a7427f597360fe52cfa8f5315e2
rune[safe] <jamesbedford@protonmail.com>
2025-04-05 00:08:49 +1100

    Completed day 6 part 1.

--- 06/main.py
+++ 06/main.py
@@ -14,7 +14,7 @@ def find_initial_position_of_guard(area_map: list[list[str]]) -> tuple[int, int]
 def part1(area_map):
     current_pos = find_initial_position_of_guard(area_map)
     current_direction = pi / 2
-    count = 1
+    visited_positions = {current_pos} # Use set to prevent from counting positions the guard has already been to twice. 
 
     while True:
         current_pos = (current_pos[0] - sin(current_direction), current_pos[1] + cos(current_direction))
@@ -22,16 +22,16 @@ def part1(area_map):
             break # The guard has exited the boundary of the mapped area.
 
         if area_map[current_pos[0]][current_pos[1]] == "#":
-            current_pos = (current_pos[0] + sin(current_direction), current_pos[1] - cos(current_direction)) # Hit obstacle so have to go back
+            current_pos = (current_pos[0] + sin(current_direction), current_pos[1] - cos(current_direction)) # Hit obstacle so have to go back one step.
             current_direction -= (pi / 2) # Direction of guard rotates 90 degrees clockwise when they hit an obstacle.
         else:
-            count += 1
+            visited_positions.add(current_pos)
     
-    return count
+    return len(visited_positions)
 
 
 def main():
-    with open("06/test_06.txt") as f: area_map = [line.strip() for line in f]
+    with open("06/input_06.txt") as f: area_map = [line.strip() for line in f]
     print(part1(area_map))
 
 

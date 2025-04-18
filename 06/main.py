ae76fcad9681d7434496a0f178542e92b6cbbd15
rune[safe] <jamesbedford@protonmail.com>
2025-04-18 23:26:17 +1000

    Continued brute force approach for day 6 part 2. Answer is too high.

--- 06/main.py
+++ 06/main.py
@@ -40,53 +40,46 @@ def part1(area_map: list[list[str]]) -> int:
 def part2(area_map: list[list[str]]) -> int:
     number_of_obstacles_to_place = 0
 
-    def follow_path(startingNavPoint: NavPoint, allow_branching=True):
-        nonlocal number_of_obstacles_to_place
-        visited_points = []
-        current_pos = startingNavPoint.position
-        current_direction = startingNavPoint.direction        
+    current_pos = find_initial_position_of_guard(area_map)
+    current_dir = pi / 2
+    visited_points = [NavPoint(current_pos, current_dir)]
 
-        while True:
-            if allow_branching:
-                print(current_pos, current_direction, "------------------------------------")
-            # else:
-            #     print(current_pos, current_direction)
+    # Get all points on the initial path excluding points adjacent to an existing obstacle.
+    while True:
+        current_pos = (current_pos[0] - sin(current_dir), current_pos[1] + cos(current_dir))
 
-            visited_points.append(NavPoint(current_pos, current_direction))
+        if current_pos[0] < 0 or current_pos[0] >= len(area_map) or current_pos[1] < 0 or current_pos[1] >= len(area_map[0]):
+            break # The guard has exited the boundary of the mapped area.
+        elif area_map[current_pos[0]][current_pos[1]] == "#":
+            current_pos = (current_pos[0] + sin(current_dir), current_pos[1] - cos(current_dir)) # Hit obstacle so have to go back one step.
+            current_dir = (current_dir - (pi / 2)) % (2 * pi) # Direction of guard rotates 90 degrees clockwise when they hit an obstacle.
+            visited_points.pop() # Remove the previous visited point since there is no point placing an obstacle on an existing obstacle.
+        else:
+            visited_points.append(NavPoint(current_pos, current_dir))
 
-            current_pos = (current_pos[0] - sin(current_direction), current_pos[1] + cos(current_direction))        
+    for point in visited_points:
+        print(point.position, point.direction)
 
-            if current_pos[0] < 0 or current_pos[0] >= len(area_map) or current_pos[1] < 0 or current_pos[1] >= len(area_map[0]):
-                return
+    for point in visited_points:
+        print(point.position, point.direction, "------------------------------")
+        current_pos = point.position
+        current_dir = (point.direction - (pi / 2)) % (2 * pi)
 
-            if area_map[current_pos[0]][current_pos[1]] == "#":
-                current_pos = (current_pos[0] + sin(current_direction), current_pos[1] - cos(current_direction))
-                current_direction = (current_direction - (pi / 2)) % (2 * pi)
+        divergent_visited_points = []
 
-            if NavPoint(current_pos, current_direction) in visited_points:
+        while True:           
+            current_pos = (current_pos[0] - sin(current_dir), current_pos[1] + cos(current_dir))
+
+            if NavPoint(current_pos, current_dir) == point or NavPoint(current_pos, current_dir) in divergent_visited_points:
                 number_of_obstacles_to_place += 1
-                if allow_branching == False:
-                    return
-                else:
-                    continue
-
-
-
-            if current_pos == startingNavPoint.position and current_direction == (startingNavPoint.direction + (pi / 2)) % (2 * pi):
-                number_of_obstacles_to_place += 1
-                if allow_branching == False:
-                    return
-                else:
-                    continue
-
-            if allow_branching:
-                follow_path(NavPoint(current_pos, (current_direction - (pi / 2)) % (2 * pi)), False)
-
-    initial_postion = find_initial_position_of_guard(area_map)
-    initial_direction = pi / 2
-
-    
-    follow_path(NavPoint(initial_postion, initial_direction))
+                break
+            elif current_pos[0] < 0 or current_pos[0] >= len(area_map) or current_pos[1] < 0 or current_pos[1] >= len(area_map[0]):
+                break
+            elif area_map[current_pos[0]][current_pos[1]] == "#":
+                current_pos = (current_pos[0] + sin(current_dir), current_pos[1] - cos(current_dir))
+                current_dir = (current_dir - (pi / 2)) % (2 * pi)
+            else:
+                divergent_visited_points.append(NavPoint(current_pos, current_dir))
 
     return number_of_obstacles_to_place
 
